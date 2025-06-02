43e4a4e858ff14730eee6e9e1c7b51c519e34586
Juan Fraguela <locofierro@users.noreply.github.com>
2025-06-02 19:00:25 -0300

    Update cosmetics.js

--- cosmetics.js
+++ cosmetics.js
@@ -1,3 +1,40 @@
+/*
+* Para carrito *
+
+$(document).ajaxComplete(function(event, xhr, settings) {
+  // Code to execute after every AJAX call
+  console.log('AJAX call completed', settings.url);
+  // Additional actions can be performed here, like updating UI elements, etc.
+});
+
+/v4/cart/add
+/v4/cart/delete
+/v4/cart/qty
+
+*/
+
+function getPageType() {
+
+    let isCheckout = window.location.pathname == '/checkout';
+
+    if (isCheckout) return 'checkout';
+
+    let isProductPage = document.getElementById("add_to_cart-btn");
+
+    let cartIsOpen = document.getElementById("cart-sidenav").checkVisibility();
+
+    return cartIsOpen 
+    
+        ? 'cart'
+
+        : isProductPage 
+        
+            ? 'product'
+
+            : 'list';
+
+}
+
 function getProductBasePrize() {
 
     let rawPrize = document.getElementsByClassName('product-vip__price-value')[0].innerHTML.trim();
@@ -8,19 +45,57 @@ function getProductBasePrize() {
 
 }
 
-function getPageType() {
+function buildProductPrizeBlock(basePrize) {
 
-    let isProductPage = document.getElementById("add_to_cart-btn");
+    let moneyParser = new Intl.NumberFormat('es-AR');
 
-    return isProductPage
+    let noDiscount = moneyParser.format(basePrize);
 
-        ? 'product'
+    let cashDiscount = moneyParser.format(basePrize - (basePrize * 0.1));
 
-        : 'list';
+    let bulkDiscount = moneyParser.format(basePrize - (basePrize * 0.2));
+
+    let result = [];
+
+    let baseLine = (prize, desc, style="") => `<p class="product-vip__price uk-flex uk-flex-middle text--primary" ${style}><span class="product-vip__price-value">$${prize} ${desc}</span></p>`;
+
+    let styleLine = 'style="background-color:#00A86B; color:white"';
+
+    result.push(baseLine(noDiscount, "tarjeta - 3 cuotas sin interes"));
+
+    result.push(baseLine(cashDiscount, "efectivo / transferencia"));
+
+    result.push(baseLine(bulkDiscount, "mayorista", styleLine));
+
+    return result;
+
+}
+
+function replaceProductPricing(basePrize) {
+
+    let block = buildProductPrizeBlock(basePrize);
+
+    let startingBlock = u(document.getElementsByClassName('product-vip__price')[0]);
+
+    startingBlock.after(block[2]);
+
+    startingBlock.after(block[1]);
+
+    startingBlock.replace(block[0]);
 
 }
 
 window.onload = (event) => {
+
+  const enabled = localStorage.getItem("ekeko");
+
+  if (!enabled)  {
+
+    console.debug("Ekeko updates disabled");
+
+    return;
+
+  }
 
   console.debug("Page loaded correctly");
 
@@ -33,7 +108,7 @@ window.onload = (event) => {
     let basePrize = getProductBasePrize();
 
     console.debug('Product base prize:', basePrize);
-    
+
   }
 
 };
